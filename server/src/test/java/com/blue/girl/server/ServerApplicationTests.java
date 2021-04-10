0dad00d1f3994074cf53fad3e2ab49ed76fd105c
msli121 <2276695595@qq.com>
2021-04-10 08:56:47 +0800

    init project

---
+++ server/src/test/java/com/blue/girl/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue.girl.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
