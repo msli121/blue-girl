0b3cf3af61ce9ccdc1b97a1366268102ed04cb0a
Scarlett12345 <1109748088@qq.com>
2021-04-11 13:35:18 +0800

    拍照

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/blue_girl_home.jpg" @click="photo"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
   },
+  methods: {
+    photo() {
+      this.$router.push({ path:'/photo'});
+    }
+  }
 };
 </script>
+<style>
+img{
+  width: 100%;
+}
+</style>
