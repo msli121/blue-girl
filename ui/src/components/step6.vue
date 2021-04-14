1920a6357f1a3c0b6d0292bcb4f682fecdc1f3bd
msli121 <2276695595@qq.com>
2021-04-14 23:11:12 +0800

    fix

--- ui/src/components/step6.vue
+++ ui/src/components/step6.vue
@@ -8,18 +8,36 @@
 </template>
 
 <script>
+  import {loadImage} from "../server/func.js"
 export default {
   name: "step6",
   data() {
-
     return {
       url: require("../assets/step-6-bg.jpg"),
       fit: "fill"
     };
   },
+  mounted() {
+    let qrCodeUrl = "https://www.performercn.com/api/file/download/141";
+    this.drawQrCodePhoto(qrCodeUrl);
+  },
   methods: {
     handleStepClick() {
       this.$router.replace({path: "/step1"})
+    },
+    async drawQrCodePhoto(qrCodeUrl) {
+      // 二维码图
+      const qrImage = await loadImage(qrCodeUrl);
+      const bgImage = await loadImage(require("../assets/step-6-bg.jpg"));
+      console.log("qrImage ",qrImage.width, qrImage.height);
+      console.log("w: ", bgImage.width, "h: ", bgImage.height);
+      let canvas = document.createElement("canvas");
+      canvas.width = bgImage.width;
+      canvas.height = bgImage.height;
+      let ctx = canvas.getContext("2d");
+      // 380 524
+      ctx.drawImage(bgImage, 0, 0, bgImage.width, bgImage.height);
+      ctx.drawImage(qrImage, 380, 524, 388, 388);
     }
   }
 }
