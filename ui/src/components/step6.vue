9962fde671d1714485981d29ac80afda8bf87f97
msli121 <2276695595@qq.com>
2021-04-15 00:56:22 +0800

    step6

--- ui/src/components/step6.vue
+++ ui/src/components/step6.vue
@@ -8,6 +8,7 @@
         @click="handleStepClick"
         class="qr-code"
         :src="url"
+        @error="imgLoadError"
         @load="imgLoad"/>
 <!--    <el-image class="qr-code"-->
 <!--      :src="url"-->
@@ -17,14 +18,16 @@
 </template>
 
 <script>
-  import {loadImage} from "@/server/func"
+  // import {loadImage} from "@/server/func";
+  import { Loading } from 'element-ui'
 export default {
   name: "step6",
   data() {
     return {
       url: require("../assets/step-6-bg.jpg"),
       fit: "fill",
-      loading: true
+      loading: true,
+      loadingInstance: null,
     };
   },
   mounted() {
@@ -33,30 +36,42 @@ export default {
     } else {
       this.url = "https://www.performercn.com/api/file/download/141";
     }
-    this.drawQrCodePhoto(this.url);
+    // 等待
+    this.loadingInstance = Loading.service(
+        { fullscreen: true }
+    )
+    // this.drawQrCodePhoto(this.url);
   },
   methods: {
     imgLoad() {
-      this.loading = false;
+      this.$nextTick(() => { // 以服务的方式调用的 Loading 需要异步关闭
+        this.loadingInstance.close();
+      });
+    },
+    imgLoadError() {
+      this.$nextTick(() => { // 以服务的方式调用的 Loading 需要异步关闭
+        this.loadingInstance.close();
+      });
+      this.$notify.warning("网络有点糟糕，再试一次吧");
     },
     handleStepClick() {
       this.$router.replace({path: "/step1"})
     },
-    async drawQrCodePhoto(qrCodeUrl) {
-      // 二维码图
-      const qrImage = await loadImage(qrCodeUrl);
-      // const bgImage = await loadImage(require("../assets/step-6-bg.jpg"));
-      console.log("qrImage ",qrImage.width, qrImage.height);
-      this.loading = false;
-      // console.log("w: ", bgImage.width, "h: ", bgImage.height);
-      // let canvas = document.createElement("canvas");
-      // canvas.width = bgImage.width;
-      // canvas.height = bgImage.height;
-      // let ctx = canvas.getContext("2d");
-      // // 380 524
-      // ctx.drawImage(bgImage, 0, 0, bgImage.width, bgImage.height);
-      // ctx.drawImage(qrImage, 380, 524, 388, 388);
-    }
+    // async drawQrCodePhoto(qrCodeUrl) {
+    //   // 二维码图
+    //   const qrImage = await loadImage(qrCodeUrl);
+    //   // const bgImage = await loadImage(require("../assets/step-6-bg.jpg"));
+    //   console.log("qrImage ",qrImage.width, qrImage.height);
+    //   this.loading = false;
+    //   // console.log("w: ", bgImage.width, "h: ", bgImage.height);
+    //   // let canvas = document.createElement("canvas");
+    //   // canvas.width = bgImage.width;
+    //   // canvas.height = bgImage.height;
+    //   // let ctx = canvas.getContext("2d");
+    //   // // 380 524
+    //   // ctx.drawImage(bgImage, 0, 0, bgImage.width, bgImage.height);
+    //   // ctx.drawImage(qrImage, 380, 524, 388, 388);
+    // }
   }
 }
 </script>
@@ -68,7 +83,6 @@ export default {
     background: url('../assets/step-6-bg.jpg') no-repeat;
     background-size: cover;
     position: absolute;
-    //background-image: 'url(' + require("../assets/step-6-bg.jpg") + ')';
 }
   .qr-code {
     z-index: 10;
