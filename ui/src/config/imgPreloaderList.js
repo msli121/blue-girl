6aa90d4e1684c89eabf7b2132c84bb2dc308cbd3
msli121 <2276695595@qq.com>
2021-04-18 01:50:09 +0800

    update: 游戏按钮问题

--- ui/src/config/imgPreloaderList.js
+++ ui/src/config/imgPreloaderList.js
@@ -1,8 +1,8 @@
 export default [
+    require('@/assets/border.png'),
     require('@/assets/step_1_bg.jpg'),
     require('@/assets/step_2_bg.jpg'),
     require('@/assets/step_6_bg.jpg'),
-    require('@/assets/border.png'),
     require('@/assets/scene_origin/scene_1.png'),
     require('@/assets/scene_origin/scene_2.png'),
     require('@/assets/scene_origin/scene_3.png'),
